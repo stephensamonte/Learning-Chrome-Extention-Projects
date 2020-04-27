a7c38b5fd134af1fc9ec5f7c54ca2ded49f4c8d3
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2020-04-27 18:22:19 -0400

    Run scrip when navigating to a youtube
    Changes:
    - Run scrip when navigating to a youtube

--- dom_retrieve_extension/background.js
+++ dom_retrieve_extension/background.js
@@ -1,19 +1,33 @@
 
-// Regex-pattern to check URLs against.
-// It matches URLs like: http[s]://[...]stackoverflow.com[...]
-var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
 
 // A function to use as callback
 function doStuffWithDom(domContent) {
-    console.log('I received the following DOM content:\n' + domContent);
+    console.log('I received the following DOM content:\n' + domContent); 
+
+    // NOTE: For YouTube DOM, it seems like "aria-label" is the identifier for video titles
 }
 
 // When the browser-action button is clicked...
 chrome.browserAction.onClicked.addListener(function (tab) {
+
+    // Regex-pattern to check URLs against.
+    // It matches URLs like: http[s]://[...]stackoverflow.com[...]
+    var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?stackoverflow\.com/;
 
     // ...check the URL of the active tab against our pattern and...
     if (urlRegex.test(tab.url)) {
         // ...if it matches, send a message specifying a callback too
         chrome.tabs.sendMessage(tab.id, {text: 'report_back'}, doStuffWithDom);
     }
+
 });
+
+// -------------------
+
+// Run function when web page loaded
+// reference: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webNavigation/onCompleted
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    // alert("This is my favorite website!");
+    // ...if it matches, send a message specifying a callback too
+    chrome.tabs.sendMessage(details.tabId, {text: 'report_back'}, doStuffWithDom);
+}, {url: [{urlMatches : 'https://www.youtube.com/'}]});
